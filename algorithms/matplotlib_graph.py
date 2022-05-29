accc46afb5fdeb649838e6e4c47778542cb8c21c
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-05-29 20:47:11 +0300

    ALL done

--- algorithms/matplotlib_graph.py
+++ algorithms/matplotlib_graph.py
@@ -1,3 +1,4 @@
+import queue
 import time 
 import numpy as np 
 from collections import defaultdict
@@ -38,8 +39,8 @@ list_of_instances=[matplolib_graph1,
                    matplotlib_graph7,
                    matplotlib_graph8]
 
-average_times=[]
-average_solutions=[]
+average_times_for_dijkstra=[]
+average_solutions_for_dijkstra=[]
 for instance in list_of_instances:
     
 
@@ -74,17 +75,214 @@ for instance in list_of_instances:
         time_taken.append(end_time - start_time)
     average_solution_length=np.sum(solution_length)/len(solution_length)
     average_time_taken=np.sum(time_taken)/len(time_taken)
-    average_times.append(average_time_taken)
-    average_solutions.append(average_solution_length)
+    average_times_for_dijkstra.append(average_time_taken)
+    average_solutions_for_dijkstra.append(average_solution_length)
 
-x_axis=np.array([0,20,30,40,50,60,70,80])
-y_axis=average_solutions
+print(average_solutions_for_dijkstra)
+print(average_times_for_dijkstra)
 
 
-plt.plot(x_axis,y_axis)
-plt.xlabel("Number of nodes")
-plt.ylabel("Solution Length")
-plt.legend("solutionLength",loc="lower right")
+
+
+
+
+matplolib_graph1=MatplolibGraph("algorithms/text_files/connections.txt")
+matplolib_graph2=MatplolibGraph("algorithms/text_files/connections2x.txt")
+matplotlib_graph3=MatplolibGraph("algorithms/text_files/connections3x.txt")
+matplotlib_graph4=MatplolibGraph("algorithms/text_files/connections4x.txt")
+matplolib_graph5=MatplolibGraph("algorithms/text_files/connections5x.txt")
+matplolib_graph6=MatplolibGraph("algorithms/text_files/connections6x.txt")
+matplotlib_graph7=MatplolibGraph("algorithms/text_files/connections7x.txt")
+matplotlib_graph8=MatplolibGraph("algorithms/text_files/connections8x.txt")
+
+list_of_instances=[matplolib_graph1,
+                   matplolib_graph2,
+                   matplotlib_graph3,
+                   matplotlib_graph4,
+                   matplolib_graph5,
+                   matplolib_graph6,
+                   matplotlib_graph7,
+                   matplotlib_graph8]
+
+
+
+def dfsFunction(start,target):
+    initial_node=start
+    stack =[initial_node]
+    visited=set(initial_node)
+    path_tracker={start:None}
+    length=0
+    while stack :
+        length+=1
+        frontier=stack .pop()
+        if frontier==target:
+                break
+        for child in graph[frontier]:
+            temp=child[0]
+            if temp not in visited:
+                visited.add(temp)
+                stack.append(temp)
+                if temp not in path_tracker:
+                    path_tracker[temp]=frontier
+    return length
+
+
+total_length=0
+average_times_for_dfs=[]
+average_solutions_for_dfs=[]
+for instance in list_of_instances:   
+    instance.file_reader()
+    graph=instance.graph
+    for key in graph:
+        graph[key]=dict(graph[key])
+        
+    all_nodes=list(graph.keys())
+    solution_length=[]
+    time_taken=[]
+    for start_node in all_nodes:
+        #We can convert a second into mili second by multiplying with
+        start_time=(time.time())
+        length=0
+        for target_node in all_nodes:
+            #If the start node is the target node, we don't need to compute a shortest distance.
+            if start_node==target_node:continue        
+            start=start_node
+            target=target_node
+            solution_len = dfsFunction(start,target)
+            length+=solution_len
+            
+            #We can change a second into milli second by multiplying with
+        end_time=(time.time())
+        solution_length.append(length/len(graph))    
+        time_taken.append(end_time - start_time)
+    average_solution_length_for_dfs=np.sum(solution_length)/len(solution_length)
+    average_time_taken_for_dfs=np.sum(time_taken)/len(time_taken)
+    average_times_for_dfs.append(average_time_taken_for_dfs)
+    average_solutions_for_dfs.append(average_solution_length_for_dfs)
+    
+average_solution_length=np.sum(solution_length)/len(solution_length)
+average_time_taken=np.sum(time_taken)/len(time_taken)
+
+print(average_solutions_for_dfs)
+# print(average_times_for_dfs)
+
+matplolib_graph1=MatplolibGraph("algorithms/text_files/connections.txt")
+matplolib_graph2=MatplolibGraph("algorithms/text_files/connections2x.txt")
+matplotlib_graph3=MatplolibGraph("algorithms/text_files/connections3x.txt")
+matplotlib_graph4=MatplolibGraph("algorithms/text_files/connections4x.txt")
+matplolib_graph5=MatplolibGraph("algorithms/text_files/connections5x.txt")
+matplolib_graph6=MatplolibGraph("algorithms/text_files/connections6x.txt")
+matplotlib_graph7=MatplolibGraph("algorithms/text_files/connections7x.txt")
+matplotlib_graph8=MatplolibGraph("algorithms/text_files/connections8x.txt")
+
+list_of_instances=[matplolib_graph1,
+                   matplolib_graph2,
+                   matplotlib_graph3,
+                   matplotlib_graph4,
+                   matplolib_graph5,
+                   matplolib_graph6,
+                   matplotlib_graph7,
+                   matplotlib_graph8]
+
+
+
+def bfsFunction(start,target):
+    initial_node=start
+    queu =queue.deque([initial_node])
+    visited=set(initial_node)
+    path_tracker={start:None}
+    length=0
+    while queu :
+        length+=1
+        frontier=queu.popleft()
+        if frontier==target:
+                break
+        for child in graph[frontier]:
+            temp=child[0]
+            if temp not in visited:
+                visited.add(temp)
+                queu.append(temp)
+                if temp not in path_tracker:
+                    path_tracker[temp]=frontier
+    return length
+
+
+total_length=0
+average_times_for_bfs=[]
+average_solutions_for_bfs=[]
+for instance in list_of_instances:   
+    instance.file_reader()
+    graph=instance.graph
+    for key in graph:
+        graph[key]=dict(graph[key])
+        
+    all_nodes=list(graph.keys())
+    solution_length=[]
+    time_taken=[]
+    for start_node in all_nodes:
+        #We can convert a second into mili second by multiplying with
+        start_time=(time.time())
+        length=0
+        for target_node in all_nodes:
+            #If the start node is the target node, we don't need to compute a shortest distance.
+            if start_node==target_node:continue        
+            start=start_node
+            target=target_node
+            solution_len = bfsFunction(start,target)
+            length+=solution_len
+            
+            #We can change a second into milli second by multiplying with
+        end_time=(time.time())
+        solution_length.append(length/len(graph))    
+        time_taken.append(end_time - start_time)
+    average_solution_length_for_dfs=np.sum(solution_length)/len(solution_length)
+    average_time_taken_for_dfs=np.sum(time_taken)/len(time_taken)
+    average_times_for_bfs.append(average_time_taken_for_dfs)
+    average_solutions_for_bfs.append(average_solution_length_for_dfs)
+    
+average_solution_length=np.sum(solution_length)/len(solution_length)
+average_time_taken=np.sum(time_taken)/len(time_taken)
+
+print(average_solutions_for_bfs)
+# print(average_times_for_bfs)
+
+
+
+
+
+
+# x_axis=np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8])
+# y_axis=average_solutions_for_dfs
+# plt.plot(x_axis,y_axis)
+
+x1=np.array([1,2,3,4,5,6,7,8])
+y1= average_solutions_for_bfs
+x2=np.array([1,2,3,4,5,6,7,8])
+y2=average_solutions_for_dfs
+x3=np.array([1,2,3,4,5,6,7,8])
+y3=average_solutions_for_dijkstra
+
+# plt.plot(x_axis,y_axis)
+
+# plt.ylim(0,100)
+plt.plot(x1, y1, label = "BFS")
+plt.plot(x2, y2, label = "DFS")
+plt.plot(x3, y3, label = "DIJKSTRA")
+plt.legend()
 plt.show()
+
+
+# x_axis=np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8])
+# y_axis=average_solutions_for_dijkstra
+# plt.plot(x_axis,y_axis)
+
+# # x_axis=np.array([1,2,3,4,5,6,7,8])
+# # y_axis=average_solutions_for_dfs
+# # plt.plot(x_axis,y_axis)
+
+# # plt.xlabel("Number of nodes")
+# # plt.ylabel("Solution Length")
+# plt.legend("solutionLength",loc="lower right")
+# plt.show()
 
 
--- algorithms/text_files/benchmark_time.py
+++ algorithms/text_files/benchmark_time.py
@@ -1,13 +1,43 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-objects = ("BFS","DFS","DIJKSTRA","A*")
-y_pos = np.arange(len(objects))
-performance = [1.26705322265625,1.0845458984375,10.639999999999997 , 10.59591064453125]
 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
+# You can draw a graph by commenting eigher of the  two
+# meaning average_solution_time and average_solution_length.
+algorithms = ("BFS","DFS","DIJKSTRA","A*")
+y = np.arange(len(algorithms))
+average_solution_time_for_bfs=1.26705322265625
+average_solution_time_for_dfs=1.0845458984375
+average_solution_time_for_dijkstra=10.639999999999997
+average_solution_time_for_A_star=10.59591064453125
+performance = [average_solution_time_for_bfs,
+               average_solution_time_for_dfs,
+               average_solution_time_for_dijkstra,
+               average_solution_time_for_A_star]
+
+plt.bar(y, performance, align='center', alpha=0.5)
+plt.xticks(y, algorithms)
 plt.ylabel('Average time')
 plt.xlabel("Algorithms")
 plt.title('Average solution time')
-
 plt.show()
+
+# algorithms = ("BFS","DFS","DIJKSTRA","A*")
+# y = np.arange(len(algorithms))
+# average_solution_length_for_bfs=11.25
+# average_solution_length_for_dfs=10.880
+# average_solution_length_for_dijkstra=10.640
+# average_solution_length_for_A_star=3.755
+# performance = [average_solution_length_for_bfs,
+#                average_solution_length_for_dfs,
+#                average_solution_length_for_dijkstra,
+#                average_solution_length_for_A_star]
+
+# plt.bar(y, performance, align='center', alpha=0.5)
+# plt.xticks(y, algorithms)
+# plt.ylabel('Average solution length')
+# plt.xlabel("Algorithms")
+# plt.title('Average solution length of those four algorithms')
+
+# plt.show()
+
+
